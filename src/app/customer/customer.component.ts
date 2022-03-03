fea49eea2c5676daa61fd67c2aed99dbb90ce433
Your Name <you@example.com>
2022-03-03 13:41:15 +0700

    initial commit

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
-import { ListService } from '../list.service';
+import { DataService } from '../service/data.service';
+import { ListService } from '../service/list.service';
+
 
 
 @Component({
@@ -11,19 +12,21 @@ import { ListService } from '../list.service';
 export class CustomerComponent implements OnInit {
   title : string ="";
   people :any[] =[
-    { id: 1, name: 'Khoa', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
-    { id: 2, name: 'Trang pupi', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
-    { id: 3, name: 'Hiep', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
-    { id: 4, name: 'Nghia', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
+    
   ]
   constructor(private listservice:ListService, private dataservice:DataService) { }
 
   ngOnInit(): void {
-    this.title = "Customer"
-    this.listservice.setCustomer(this.people);
- 
-   
+    this.title = "Customer";
+    this.people=[
+      { id: 1, name: 'Khoa', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
+    { id: 2, name: 'Trang pupi', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
+    { id: 3, name: 'Hiep', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
+    { id: 4, name: 'Nghia', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
+    ]
+    
   }
+  
  
   
 
--- src/app/list-customer/list-customer.component.ts
+++ src/app/list-customer/list-customer.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ListService } from '../list.service';
+import { Component, OnInit,Input } from '@angular/core';
+import { ListService } from '../service/list.service';
+import { ICustomer } from './share/interfaces';
 
 @Component({
   selector: 'app-list-customer',
@@ -7,22 +8,43 @@ import { ListService } from '../list.service';
   styleUrls: ['./list-customer.component.css']
 })
 export class ListCustomerComponent implements OnInit {
-  listcustomer :any[] = []
+  private _customer :ICustomer[] = [] ;
+  @Input() get customers(): ICustomer[] {
+    return this._customer;
+}
+ set customers( value: ICustomer[]){
+   if(value){
+     this.listcustomer = this._customer= value;
+   }
+ }
+ returnCustomer(){
+   this.listcustomer = this._customer;
+ }
+
+
+
+
+
+  listcustomer :any[] = [];
   coin :string = "USD";
-  names: string = ""
+  names: string = "";
 
   constructor(private listservice:ListService) { }
 
   ngOnInit(): void {
-    this.listcustomer = this.listservice.getCustomer()
+    
   }
+
+
+
+
   search(){
-    if(this.names!=""){
+    if(this.names!==""){
       this.listcustomer = this.listcustomer.filter(cust=>{
         return cust.name.toLocaleLowerCase().match(this.names.toLocaleLowerCase())
       })
     }else if(this.names==""){
-      this.ngOnInit()
+    this.returnCustomer()
     }
     
   }
